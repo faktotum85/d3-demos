f7e8b4db0d0b6a9e7bf5a5ebd22b1d0fdffa95dd
Simon Weller <s.meierbeck@gmail.com>
2017-10-02 23:26:24 +0200

    added responsive sizing

--- public/main.js
+++ public/main.js
@@ -1,56 +1,48 @@
 (function() {
 
+  var dataset, xScale, yScale, xAxis, yAxis, svg, div, padding, barWidth, formatTime;
+
   axios
     .get('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json')
     .then(function(res) {
-      init(res.data.data.map(function(entry) {return [new Date(entry[0]), entry[1]]}));
+      dataset = res.data.data.map(function(entry) {return [new Date(entry[0]), entry[1]]});
+      init();
     })
     .catch(function (err) {
       console.error(err);
     });
 
-  function init(dataset) {
+  window.addEventListener('resize', render);
 
-    var chartStyle = window.getComputedStyle(document.querySelector('#chart'), null);
-    var padding = 60;
-    var barWidth = 3;
-    var w = window;
-    var d = document;
-    var e = d.documentElement;
-    var g = d.getElementsByTagName('body')[0];
-    var width = Math.min((w.innerWidth || e.clientWidth || g.clientWidth), 960);
-    var height = (w.innerHeight|| e.clientHeight|| g.clientHeight) - padding - 200;
+  function init() {
 
-    var formatTime = d3.timeFormat('%Y - %b');
-
-    var xScale = d3.scaleTime()
-                   .range([padding, width - padding]);
-    xScale.domain(d3.extent(dataset, function (d) {return d[0]}));
-
-    var yScale = d3.scaleLinear()
-                   .domain([0, d3.max(dataset, function(d) {return d[1]})])
-                   .range([height - padding, padding]);
+    padding = 60;
+    barWidth = 3;
+    formatTime = d3.timeFormat('%Y - %b');
 
     // define the div for the tooltip
-    var div = d3.select('#chart').append('div')
+    div = d3.select('#chart').append('div')
       .attr('class', 'tooltip')
       .style('opacity', 0);
 
-    var svg = d3.select('#chart')
+    // define svg
+    svg = d3.select('#chart')
       .append('svg')
-      .attr('width', width)
-      .attr('height', height)
-      .attr('x', 0)
-      .attr('y', 0)
+
+    xScale = d3.scaleTime().domain(d3.extent(dataset, function (d) {return d[0]}));
+    yScale = d3.scaleLinear().domain([0, d3.max(dataset, function(d) {return d[1]})]);
+
+    xAxis = d3.axisBottom(xScale);
+    yAxis = d3.axisLeft(yScale);
+
+    svg.append('g').classed('x axis', true);
+    svg.append('g').classed('y axis', true);
 
     svg.selectAll('rect')
       .data(dataset)
       .enter()
       .append('rect')
-      .attr('x', function(d, i) {return xScale(d[0]) })
-      .attr('y', function(d, i) {return yScale(d[1]) })
       .attr('width', barWidth)
-      .attr('height', function(d, i) {return height - padding - yScale(d[1])})
       .attr('fill', 'lightsteelblue')
       .on('mouseover', function(d) {
         d3.select(this)
@@ -72,10 +64,40 @@
           .attr('fill', 'lightsteelblue')
       });
 
+      render();
+  }
 
-    var xAxis = d3.axisBottom(xScale);
+  function render() {
 
-    svg.append('g')
+    var w = window;
+    var d = document;
+    var e = d.documentElement;
+    var g = d.getElementsByTagName('body')[0];
+    var width = Math.min((w.innerWidth || e.clientWidth || g.clientWidth), 960);
+    var height = (w.innerHeight|| e.clientHeight|| g.clientHeight) - padding - 200;
+
+    xScale.range([padding, width - padding]);
+
+    yScale.range([height - padding, padding]);
+
+    svg.attr('width', width)
+      .attr('height', height)
+
+    svg.selectAll('rect')
+      .attr('x', function(d, i) {return xScale(d[0]) })
+      .attr('y', function(d, i) {return yScale(d[1]) })
+      .attr('height', function(d, i) {return height - padding - yScale(d[1])})
+
+    // adjust tick based on window size
+    if (width < 400) {
+      xAxis.ticks(d3.timeYear.every(15));
+    } else if (width < 750) {
+      xAxis.ticks(d3.timeYear.every(10));
+    } else {
+      xAxis.ticks(d3.timeYear.every(5));
+    }
+
+    svg.select('.x.axis')
      .attr('transform', 'translate(0, ' + (height - padding) + ')')
      .call(xAxis)
      .selectAll('.tick>text')
@@ -83,15 +105,15 @@
        d3.select(this).style('font-size', '16px')
      });
 
-    var yAxis = d3.axisLeft(yScale);
-
-    svg.append('g')
+    svg.select('.y.axis')
       .attr('transform', 'translate(' + padding + ', 0)')
       .call(yAxis)
       .selectAll('.tick>text')
       .each(function(d, i) {
         d3.select(this).style('font-size', '16px')
       });
+
+
   }
 
 })()
