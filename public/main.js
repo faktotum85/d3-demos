d383025139e08362e07479ef4c65581a22b0b05f
Simon Weller <s.meierbeck@gmail.com>
2017-10-03 09:47:49 +0200

    tweaks to responsiveness (tooltip position & height)

--- public/main.js
+++ public/main.js
@@ -1,6 +1,6 @@
 (function() {
 
-  var dataset, xScale, yScale, xAxis, yAxis, svg, div, padding, barWidth, formatTime;
+  var dataset, xScale, yScale, xAxis, yAxis, svg, div, margin, barWidth, formatTime, width, height;
 
   axios
     .get('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json')
@@ -16,7 +16,12 @@
 
   function init() {
 
-    padding = 60;
+    margin = {
+      left: 60,
+      right: 20,
+      top: 20,
+      bottom: 20
+    }
     barWidth = 3;
     formatTime = d3.timeFormat('%Y - %b');
 
@@ -29,6 +34,7 @@
     svg = d3.select('#chart')
       .append('svg')
 
+    // Initialize and add axes
     xScale = d3.scaleTime().domain(d3.extent(dataset, function (d) {return d[0]}));
     yScale = d3.scaleLinear().domain([0, d3.max(dataset, function(d) {return d[1]})]);
 
@@ -38,6 +44,7 @@
     svg.append('g').classed('x axis', true);
     svg.append('g').classed('y axis', true);
 
+    // Put in data and add listeners
     svg.selectAll('rect')
       .data(dataset)
       .enter()
@@ -53,8 +60,15 @@
         div.html(
             '<strong>' + d[1].toLocaleString('en-US', {style: 'currency', currency: 'USD'})
             + ' Billion</strong><br/>' + formatTime(d[0]))
-          .style('left', (d3.event.pageX + 10) + 'px')
-          .style('top', (d3.event.pageY - 30) + 'px');
+        if (width - d3.event.pageX < 125) { // near the right edge
+          div
+            .style('left', (d3.event.pageX - 150) + 'px')
+            .style('top', (d3.event.pageY - 30) + 'px');
+        } else {
+          div
+            .style('left', (d3.event.pageX + 10) + 'px')
+            .style('top', (d3.event.pageY - 30) + 'px');
+        }
       })
       .on('mouseout', function(d) {
         div.transition()
@@ -73,20 +87,8 @@
     var d = document;
     var e = d.documentElement;
     var g = d.getElementsByTagName('body')[0];
-    var width = Math.min((w.innerWidth || e.clientWidth || g.clientWidth), 960);
-    var height = (w.innerHeight|| e.clientHeight|| g.clientHeight) - padding - 200;
-
-    xScale.range([padding, width - padding]);
-
-    yScale.range([height - padding, padding]);
-
-    svg.attr('width', width)
-      .attr('height', height)
-
-    svg.selectAll('rect')
-      .attr('x', function(d, i) {return xScale(d[0]) })
-      .attr('y', function(d, i) {return yScale(d[1]) })
-      .attr('height', function(d, i) {return height - padding - yScale(d[1])})
+    width = Math.min((w.innerWidth || e.clientWidth || g.clientWidth), 960) - 30 ;
+    height = (w.innerHeight|| e.clientHeight|| g.clientHeight) - 150;
 
     // adjust tick based on window size
     if (width < 400) {
@@ -97,8 +99,28 @@
       xAxis.ticks(d3.timeYear.every(5));
     }
 
+    // adjust height for lower screens (e.g. landscape)
+    if (height < 300) {
+      height+= 50;
+    }
+
+    // adjust sizing of chart area...
+    svg.attr('width', width)
+      .attr('height', height)
+
+    // scales
+    xScale.range([margin.left, width - margin.right]);
+    yScale.range([height - margin.bottom, margin.top]);
+
+    // and bars
+    svg.selectAll('rect')
+      .attr('x', function(d, i) {return xScale(d[0]) })
+      .attr('y', function(d, i) {return yScale(d[1]) })
+      .attr('height', function(d, i) {return height - margin.bottom - yScale(d[1])})
+
+    // and adjust axes
     svg.select('.x.axis')
-     .attr('transform', 'translate(0, ' + (height - padding) + ')')
+     .attr('transform', 'translate(0, ' + (height - margin.bottom) + ')')
      .call(xAxis)
      .selectAll('.tick>text')
      .each(function(d, i) {
@@ -106,14 +128,12 @@
      });
 
     svg.select('.y.axis')
-      .attr('transform', 'translate(' + padding + ', 0)')
+      .attr('transform', 'translate(' + margin.left + ', 0)')
       .call(yAxis)
       .selectAll('.tick>text')
       .each(function(d, i) {
         d3.select(this).style('font-size', '16px')
       });
-
-
   }
 
 })()
