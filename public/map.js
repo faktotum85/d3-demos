96d70b27942b5a06cc31f230904190e96feaaaf6
Simon Weller <s.meierbeck@gmail.com>
2017-10-05 21:12:35 +0200

    drawing meteor strike map

--- public/map.js
+++ public/map.js
@@ -12,8 +12,8 @@
 
   axios.all([getStrikes(), getMap()])
     .then(axios.spread(function(strikes, geoMap) {
-      meteorStrikes = strikes;
-      map = geoMap;
+      meteorStrikes = strikes.data;
+      map = geoMap.data;
       init();
     }))
     .catch(function(err) {console.error(err)});
@@ -22,20 +22,27 @@
     var width = 700;
     var height = 580;
 
-    console.log(meteorStrikes);
-    console.log(map);
 
-    // var neighborhoods = svg.append('g');
-    //
+    var svg = d3.select('#chart').append('svg')
+                .attr({'width': width, 'height': height});
+
+    var countries = svg.append('g');
+
     // // a projection creates a function into which you can plug longitude and latitude values and get projected coordinates back.
     var mercProjection = d3.geoMercator()
-    //
+
     // // A geo path is a function that takes a GeoJSON feature and returns SVG path data, based on the specified projection.
     var geoPath = d3.geoPath().projection(mercProjection);
     // var geoPath = d3.geoPath()
     //   .pointRadius(function(d) {return d.properties.precinct / 100}) // Optionally you can specify a pointRadius to control the circle size
     //   .projection(albersProjection);
     //
+    countries.selectAll('path')
+      .data(map.features)
+      .enter()
+      .append('path')
+      .attr('fill', '#ccc')
+      .attr('d', geoPath);
     // neighborhoods.selectAll('path')
     //   .data(neighborhoods_json.features)
     //   .enter()
@@ -53,8 +60,7 @@
     //   .attr('stroke', '#999')
     //   .attr('d', geoPath); // When a path generator encounters point features, it draws a circle.
 
-    var svg = d3.select('#chart').append('svg')
-                .attr({'width': width, 'height': height});
+
 
   }
 
